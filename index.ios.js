c591d7ab61258414a85647df90db9c9e7b828e11
Adrian Philipp <mail@adrian-philipp.com>
2016-02-04 19:27:01 +0100

    Adds draft

---
+++ index.ios.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MultiSlider from 'react-native-multi-slider';
+
+
+class react_native_multi_slider_example extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <MultiSlider
+          min={0}
+          max={99}
+          values={[10, 99]}
+          sliderLength={300}
+        />
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('react_native_multi_slider_example', () => react_native_multi_slider_example);
